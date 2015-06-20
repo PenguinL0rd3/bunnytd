1c5597e6f8b6b4cbea3cb35c79cef51fa1e2e6c0
LK Student <lkstudent@Tonys-iMac.local>
2015-06-19 17:41:23 -0700

    Began To Crate Beaves And Fixed Arrows/White Space

--- game.py
+++ game.py
@@ -6,7 +6,11 @@ pygame.init()
 width, height = 640, 480
 screen=pygame.display.set_mode((width, height))
 acc=[0,0]
-arrow=[]
+arrows=[]
+badtimer=100
+badtimer1=0
+badguys=[[640,100]]
+healthvalue=194
 
 keys = [False, False, False, False]
 playerpos=[100,100]
@@ -17,75 +21,82 @@ grass = pygame.image.load("resources/images/grass.png")
 castle = pygame.image.load("resources/images/castle.png")
 arrow = pygame.image.load("resources/images/bullet.png")
 
+badguyimg1 = pygame.image.load("resources/images/badguy.png")
+badguyimg=badguyimg1
+
 while 1:
 
-	screen.fill(0)
+    #screen.fill(0)
 
-	for x in range(width/grass.get_width()+1):
-		for y in range(height/grass.get_height()+1):
-			screen.blit(grass,(x*100,y*100))
-	screen.blit(castle,(0,30))
-	screen.blit(castle,(0,135))
-	screen.blit(castle,(0,240))
-	screen.blit(castle,(0,345))
+    for x in range(width/grass.get_width()+1):
+        for y in range(height/grass.get_height()+1):
+            screen.blit(grass,(x*100,y*100))
+    screen.blit(castle,(0,30))
+    screen.blit(castle,(0,135))
+    screen.blit(castle,(0,240))
+    screen.blit(castle,(0,345))
 
-	position = pygame.mouse.get_pos()
-	angle = math.atan2(position[1]-(playerpos[1]+32),position[0]-(playerpos[0]+26))
-	playerrot = pygame.transform.rotate(player, 360-angle*57.29)
-	playerpos1 = (playerpos[0]-playerrot.get_rect().width/2, playerpos[1]-playerrot.get_rect().height/2)
-	screen.blit(playerrot, playerpos1)
+    position = pygame.mouse.get_pos()
+    angle = math.atan2(position[1]-(playerpos[1]+32),position[0]-(playerpos[0]+26))
+    playerrot = pygame.transform.rotate(player, 360-angle*57.29)
+    playerpos1 = (playerpos[0]-playerrot.get_rect().width/2, playerpos[1]-playerrot.get_rect().height/2)
+    screen.blit(playerrot, playerpos1)
 
-        for bullet in arrows:
-                index=0
-                velx=math.cos(bullet[0])*10
-                vely=math.sin(bullet[0])*10
-                bullet[1]+=velx
-                bullet[2]+=vely
-                if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
-                        arrows.pop(index)
-                index+=1
-                for projectile in arrows:
-                        arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
-                        screen.blit(arrow1, (projectile[1], projectile[2]))
+    for bullet in arrows:
+        index=0
+        velx=math.cos(bullet[0])*10
+        vely=math.sin(bullet[0])*10
+        bullet[1]+=velx
+        bullet[2]+=vely
+        if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
+            arrows.pop(index)
+        index+=1
+        for projectile in arrows:
+            arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
+            screen.blit(arrow1, (projectile[1], projectile[2]))
 
-	pygame.display.flip()
+    if badtimer==0
+        badguys.append([640, random.randint(50,430)])
+        
 
-	for event in pygame.event.get():
-		
-		if event.type==pygame.MOUSEBUTTONDOWN:
-			position=pygame.mouse.get_pos()
-			acc[1]+=1
-			arrows.append([math.atan2(position[1]-(playerpos1[1]+32),position[0]-(playerpos1[0]+26)),playerpos1[0]+32,playerpos1[1]+32])
+    pygame.display.flip()
 
-		if event.type==pygame.QUIT:
-	
-			pygame.quit()
-			exit(0)
+    for event in pygame.event.get():
+        
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            position=pygame.mouse.get_pos()
+            acc[1]+=1
+            arrows.append([math.atan2(position[1]-(playerpos1[1]+32),position[0]-(playerpos1[0]+26)),playerpos1[0]+32,playerpos1[1]+32])
 
-		if event.type == pygame.KEYDOWN:
-                        if event.key==K_w:
-                                keys[0]=True
-			elif event.key==K_a:
-				keys[1]=True
-			elif event.key==K_s:
-				keys[2]=True
-			elif event.key==K_d:
-				keys[3]=True
-		if event.type == pygame.KEYUP:
-			if event.key==pygame.K_w:
-				keys[0]=False
-			elif event.key==pygame.K_a:
-				keys[1]=False
-			elif event.key==pygame.K_s:
-				keys[2]=False
-			elif event.key==pygame.K_d:
-				keys[3]=False
-		  
-	if keys[0]:
-		playerpos[1]-=5
-	elif keys[2]:
-		playerpos[1]+=5
-	if keys[1]:
-		playerpos[0]-=5
-	elif keys[3]:
-		playerpos[0]+=5
+        if event.type==pygame.QUIT:
+    
+            pygame.quit()
+            exit(0)
+
+        if event.type == pygame.KEYDOWN:
+            if event.key==K_w:
+                keys[0]=True
+            elif event.key==K_a:
+                keys[1]=True
+            elif event.key==K_s:
+                keys[2]=True
+            elif event.key==K_d:
+                keys[3]=True
+        if event.type == pygame.KEYUP:
+            if event.key==pygame.K_w:
+                keys[0]=False
+            elif event.key==pygame.K_a:
+                keys[1]=False
+            elif event.key==pygame.K_s:
+                keys[2]=False
+            elif event.key==pygame.K_d:
+                keys[3]=False
+          
+    if keys[0]:
+        playerpos[1]-=5
+    elif keys[2]:
+        playerpos[1]+=5
+    if keys[1]:
+        playerpos[0]-=5
+    elif keys[3]:
+        playerpos[0]+=5

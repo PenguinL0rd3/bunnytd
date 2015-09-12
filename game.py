e6e0182471bc4ea4a5ec870c04a5210dbfccdb46
Nichodon <brandonsyc.2222@yahoo.com>
2015-09-11 18:15:03 -0700

    added beavers and health bar, fix upper left corner graphical glitch

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
-import pygame
+import pygame # Open in 32 bit version of python only.
 from pygame.locals import *
 import math
+import random
 
 pygame.init()
 width, height = 640, 480
@@ -24,7 +25,11 @@ arrow = pygame.image.load("resources/images/bullet.png")
 badguyimg1 = pygame.image.load("resources/images/badguy.png")
 badguyimg=badguyimg1
 
+healthbar = pygame.image.load("resources/images/healthbar.png")
+health = pygame.image.load("resources/images/health.png")
+
 while 1:
+    badtimer-=1
 
     #screen.fill(0)
 
@@ -55,10 +60,48 @@ while 1:
             arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
             screen.blit(arrow1, (projectile[1], projectile[2]))
 
-    if badtimer==0
+    if badtimer==0:
         badguys.append([640, random.randint(50,430)])
-        
-
+        badtimer=100-(badtimer1*2)
+        if badtimer1>=35:
+            badtimer1=35
+        else:
+            badtimer1+=5
+    index=0
+    for badguy in badguys:
+        if badguys[0]<-64:
+            badguys.pop(index)
+        badguy[0]-=7
+        badrect=pygame.Rect(badguyimg.get_rect())
+        badrect.top=badguy[1]
+        badrect.left=badguy[0]
+        if badrect.left<64:
+            healthvalue -= random.randint(5,20)
+            badguys.pop(index)
+        index1=0
+        for bullet in arrows:
+            bullrect=pygame.Rect(arrow.get_rect())
+            bullrect.left=bullet[1]
+            bullrect.top=bullet[2]
+            if badrect.colliderect(bullrect):
+                acc[0]+=1
+                badguys.pop(index)
+                arrows.pop(index1)
+            index1+=1
+        index+=1
+    for badguy in badguys:
+        screen.blit(badguyimg, badguy)
+    font = pygame.font.Font(None, 24)
+    survivedtext = font.render(str((90000-
+pygame.time.get_ticks())/60000)+":"+str((90000-
+pygame.time.get_ticks())/1000%60).zfill(2), True, (0,0,0))
+    textRect = survivedtext.get_rect()
+    textRect.topright=[635,5]
+    screen.blit(survivedtext, textRect)
+    screen.blit(healthbar, (5,5))
+    for health1 in range(healthvalue):
+        screen.blit(health, (health1+8,8))
+    
     pygame.display.flip()
 
     for event in pygame.event.get():
@@ -66,7 +109,8 @@ while 1:
         if event.type==pygame.MOUSEBUTTONDOWN:
             position=pygame.mouse.get_pos()
             acc[1]+=1
-            arrows.append([math.atan2(position[1]-(playerpos1[1]+32),position[0]-(playerpos1[0]+26)),playerpos1[0]+32,playerpos1[1]+32])
+            arrows.append([math.atan2(position[1]-(playerpos1[1]+32),position[0]-
+(playerpos1[0]+26)),playerpos1[0]+32,playerpos1[1]+32])
 
         if event.type==pygame.QUIT:
     
